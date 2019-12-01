6dd4d1e8d7e27d0b837f1bcd7006391998c825b1
slametrianto <slametrianto25@gmail.com>
2019-12-01 09:18:34 +0700

    revisi

---
+++ application/views/templ/auth_header.php
@@ -0,0 +1,35 @@
+<html lang="en">
+
+</html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- <link rel="icon" href="<?php echo base_url(); ?>assets/img/icon.png" type="image/icon"> -->
+
+    <link rel="shortcut icon" type="image/png" href="assets/img/kos.png" />
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://raw.githubusercontent.com/daneden/animate.css/master/animate.css">
+    <link rel="stylesheet" href="assets/css/bootnavbar.css">
+
+
+
+
+    <title>Energi Termal Solusi</title>
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+
+
+
+
+    <meta name="description" content="Multi level hover dropdown Navbar for bootstrap 4">
+    <meta name="keywords" content="Multi level hover dropdown Navbar for bootstrap 4">
+</head>
+
+<body>
